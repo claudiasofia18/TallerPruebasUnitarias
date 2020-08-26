ee18c30c97b88a14004f8a81a53cd764f4b6e662
Xavier Garcia <xpgarcia@espol.edu.ec>
2020-08-26 13:01:45 -0500

    Nuevos tests agregado

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -2,6 +2,10 @@ package test;
 import Taller.*;
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,67 +14,104 @@ public class EmployeeTest {
 	
 	private Employee SupervisorUSD;
 	private Employee SupervisorNoUSD;
+	private Employee WorkerUSD;
+	private Employee WorkerNoUSD;
 	
 	@Before
     public void init() {
 		SupervisorUSD =  new Employee(456.68F, "USD", 5.5F, EmployeeType.Supervisor);
 		SupervisorNoUSD = new Employee(456.68F, "WON", 5.5F, EmployeeType.Supervisor);
-        
+		WorkerUSD = new Employee(1000,"USD",0.1f,EmployeeType.Worker);
+        WorkerNoUSD = new Employee(1000,"EU",0.1f,EmployeeType.Worker);
     }
 	
-	/*
+	/**
 	 * Este caso de prueba evalúa el cálculo del salario
-	 * para el tipo de empleado "Supervisor" cuando el tipo de moneda es USD 
-	 * y el mes es impar, por lo que también recibe el décimo junto con su 
-	 * salario y un bono.
+	 * para el tipo de empleado "Supervisor" cuando el tipo de moneda es USD. 
+	 * Cuando el mes es PAR, el empleado NO recibe decimo.
+	 * Cuando el mes es IMPAR, el empleado SI recibe decimo.
 	 */
-	
 	@Test
 	public void csSupervisorUSDDecimoFinal() {
 		float salario = SupervisorUSD.cs();
-		assertEquals(522.938, salario,0.001);		
+		float valorEsperado;
+		Date date = new Date();
+		LocalDate localDate;
+	    localDate = date.toInstant()
+	            .atZone(ZoneId.systemDefault()).toLocalDate();
+	    int month = localDate.getMonthValue();
+	    if(month%2==0) {
+	    	valorEsperado = 458.605f;
+	    }else {
+	    	valorEsperado = 522.938f;
+	    }
+		assertEquals(valorEsperado, salario, 0.001);		
 	}
 	
-	
-	/*
+	/**
 	 * Este caso de prueba evalúa el cálculo del salario
-	 * para el tipo de empleado "Supervisor" cuando el tipo de moneda es USD 
-	 * y el mes es par.
+	 * para el tipo de empleado "Supervisor" cuando el tipo de moneda NO es USD. 
+	 * Cuando el mes es PAR, el empleado NO recibe decimo.
+	 * Cuando el mes es IMPAR, el empleado SI recibe decimo.
 	 */
-	
-	@Test
-	public void csSupervisorUSDNoDecimoFinal() {
-		float salario = SupervisorUSD.cs();
-		assertEquals(458.605, salario,0.001);		
-	}
-	
-	
-	/*
-	 * Este caso de prueba evalúa el cálculo del salario
-	 * para el tipo de empleado "Supervisor" cuando el tipo de moneda no es USD 
-	 * y el mes es impar, por lo que también recibe el décimo junto con su 
-	 * salario y un bono.
-	 */
-	
 	@Test
 	public void csSupervisortestNoUSDDecimoFinal() {
 		float salary = SupervisorNoUSD.cs();
-		assertEquals(500.104, salary,0.001);		
+		float valorEsperado;
+		Date date = new Date();
+		LocalDate localDate;
+	    localDate = date.toInstant()
+	            .atZone(ZoneId.systemDefault()).toLocalDate();
+	    int month = localDate.getMonthValue();
+	    if(month%2==0) {
+	    	valorEsperado = 435.771f;
+	    }else {
+	    	valorEsperado = 500.104f;
+	    }
+		assertEquals(valorEsperado, salary, 0.001);		
 	}
 	
-	/*
+	/**
 	 * Este caso de prueba evalúa el cálculo del salario
-	 * para el tipo de empleado "Supervisor" cuando el tipo de moneda no es USD 
-	 * y el mes es par.
+	 * para un tipo de empleado "Worker", cuando el tiempo de moneda SI es USD.
+	 * Cuando el mes es PAR, el empleado NO recibe decimo.
+	 * Cuando el mes es IMPAR, el empleado SI recibe decimo.
 	 */
-	
 	@Test
-	public void csSupervisortestNoUSDNoDecimoFinal() {
-		float salary = SupervisorNoUSD.cs();
-		assertEquals(435.771, salary,0.001);		
+	public void csUSDWorkerDecimoTest() {
+		float valorEsperado;
+		Date date = new Date();
+		LocalDate localDate;
+	    localDate = date.toInstant()
+	            .atZone(ZoneId.systemDefault()).toLocalDate();
+	    int month = localDate.getMonthValue();
+	    if(month%2==0) {
+	    	valorEsperado = 1000;
+	    }else {
+	    	valorEsperado = 1016.083f;
+	    }
+		assertEquals(valorEsperado, WorkerUSD.cs(), 0.0001);
 	}
 	
-
-	
-
+	/**
+	 * Este caso de prueba evalúa el cálculo del salario
+	 * para un tipo de empleado "Worker", cuando el tiempo de moneda NO es USD.
+	 * Cuando el mes es PAR, el empleado NO recibe decimo.
+	 * Cuando el mes es IMPAR, el empleado SI recibe decimo.
+	 */
+	@Test
+	public void csNoUSDWorkerDecimoTest() {
+		float valorEsperado;
+		Date date = new Date();
+		LocalDate localDate;
+	    localDate = date.toInstant()
+	            .atZone(ZoneId.systemDefault()).toLocalDate();
+	    int month = localDate.getMonthValue();
+	    if(month%2==0) {
+	    	valorEsperado = 950;
+	    }else {
+	    	valorEsperado = 966.083f;
+	    }
+		assertEquals(valorEsperado, WorkerNoUSD.cs(), 0.0001);
+	}
 }
